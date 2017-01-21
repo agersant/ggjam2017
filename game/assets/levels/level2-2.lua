8e3a563951aa1f9e79c10d7de206822be722cb72
Selcopa <selcopa@gmail.com>
2017-01-20 23:21:40 -0800

    Levels
    added first levels

---
+++ game/assets/levels/level2-2.lua
@@ -0,0 +1,253 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.18.0",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 20,
+  height = 20,
+  tilewidth = 28,
+  tileheight = 28,
+  nextobjectid = 17,
+  properties = {},
+  tilesets = {},
+  layers = {
+    {
+      type = "objectgroup",
+      name = "Object Layer 1",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      draworder = "topdown",
+      properties = {},
+      objects = {
+        {
+          id = 2,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 0,
+          y = 224,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0"
+          }
+        },
+        {
+          id = 3,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 56,
+          y = 168,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0"
+          }
+        },
+        {
+          id = 4,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 112,
+          y = 112,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0"
+          }
+        },
+        {
+          id = 5,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 140,
+          y = 56,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "1"
+          }
+        },
+        {
+          id = 6,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 196,
+          y = 28,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "1"
+          }
+        },
+        {
+          id = 7,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 280,
+          y = 28,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "1"
+          }
+        },
+        {
+          id = 8,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 336,
+          y = 56,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "2"
+          }
+        },
+        {
+          id = 9,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 392,
+          y = 112,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "2"
+          }
+        },
+        {
+          id = 10,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 420,
+          y = 168,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "2"
+          }
+        },
+        {
+          id = 11,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 364,
+          y = 224,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "3"
+          }
+        },
+        {
+          id = 12,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 280,
+          y = 280,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "3"
+          }
+        },
+        {
+          id = 13,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 224,
+          y = 336,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "3"
+          }
+        },
+        {
+          id = 14,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 140,
+          y = 336,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "4"
+          }
+        },
+        {
+          id = 15,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 84,
+          y = 308,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "4"
+          }
+        },
+        {
+          id = 16,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 28,
+          y = 280,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "4"
+          }
+        }
+      }
+    }
+  }
+}
---
+++ game/assets/levels/level3-1.lua
@@ -0,0 +1,253 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.18.0",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 20,
+  height = 20,
+  tilewidth = 28,
+  tileheight = 28,
+  nextobjectid = 17,
+  properties = {},
+  tilesets = {},
+  layers = {
+    {
+      type = "objectgroup",
+      name = "Object Layer 1",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      draworder = "topdown",
+      properties = {},
+      objects = {
+        {
+          id = 2,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 224,
+          y = 28,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0"
+          }
+        },
+        {
+          id = 3,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 168,
+          y = 84,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0"
+          }
+        },
+        {
+          id = 4,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 112,
+          y = 168,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0"
+          }
+        },
+        {
+          id = 5,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 308,
+          y = 224,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "3"
+          }
+        },
+        {
+          id = 6,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 224,
+          y = 224,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "1"
+          }
+        },
+        {
+          id = 7,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 140,
+          y = 308,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "1"
+          }
+        },
+        {
+          id = 8,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 84,
+          y = 392,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "1"
+          }
+        },
+        {
+          id = 9,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 168,
+          y = 448,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "2"
+          }
+        },
+        {
+          id = 10,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 252,
+          y = 504,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "2"
+          }
+        },
+        {
+          id = 11,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 364,
+          y = 448,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "2"
+          }
+        },
+        {
+          id = 12,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 448,
+          y = 392,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "3"
+          }
+        },
+        {
+          id = 13,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 392,
+          y = 308,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "3"
+          }
+        },
+        {
+          id = 14,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 420,
+          y = 168,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "4"
+          }
+        },
+        {
+          id = 15,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 392,
+          y = 84,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "4"
+          }
+        },
+        {
+          id = 16,
+          name = "Collect",
+          type = "pickup",
+          shape = "rectangle",
+          x = 336,
+          y = 0,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "4"
+          }
+        }
+      }
+    }
+  }
+}
