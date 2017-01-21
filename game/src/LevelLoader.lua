95c110b71f97e6225517a627664035e6b45f78d2
Devin Curry - Laptop <Devination3D@gmail.com>
2017-01-21 00:16:35 -0800

    Add 'chunk' and 'ent' data to Pickups

--- game/src/LevelLoader.lua
+++ game/src/LevelLoader.lua
@@ -4,7 +4,9 @@ local LevelLoader = Class("LevelLoader");
 LevelLoader.init = function(self)
 end
 
---Reads a list of objects from a Tiled level and returns a table of pickup positions
+--Reads a list of objects from a Tiled level and returns a table of pickup positions and chunks
+--level.pickups[1].ent -- used for sorting (pickups need to be collected in order)
+--level.pickups[1].chunk
 --level.pickups[1].x
 --level.pickups[1].y
 LevelLoader.loadLevel = function(self, levelName)
@@ -14,7 +16,12 @@ LevelLoader.loadLevel = function(self, levelName)
 
     local objects = levelData.layers[1].objects;
     for i in pairs(objects) do
-        table.insert(entities.pickups, {x = objects[i].x, y = objects[i].y})
+        table.insert(entities.pickups, {
+                ent = objects[i].properties['ent'],
+                chunk = objects[i].properties['chunk'],
+                x = objects[i].x,
+                y = objects[i].y
+            })
     end
     return entities;
 end
