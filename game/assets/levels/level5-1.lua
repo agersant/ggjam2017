4503c3c82de0d76556f4e96d95d0b060ff06afda
Selcopa <selcopa@gmail.com>
2017-01-21 01:55:14 -0800

    Level 5 player 1
    lvl 5 player 1

---
+++ game/assets/levels/level5-1.lua
@@ -0,0 +1,172 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.18.0",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 20,
+  height = 20,
+  tilewidth = 28,
+  tileheight = 28,
+  nextobjectid = 10,
+  properties = {},
+  tilesets = {},
+  layers = {
+    {
+      type = "objectgroup",
+      name = "Object Layer 1",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      draworder = "topdown",
+      properties = {},
+      objects = {
+        {
+          id = 1,
+          name = "Collect0",
+          type = "pickup",
+          shape = "rectangle",
+          x = 392,
+          y = 84,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0",
+            ["ent"] = "0"
+          }
+        },
+        {
+          id = 2,
+          name = "Collect1",
+          type = "pickup",
+          shape = "rectangle",
+          x = 420,
+          y = 308,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0",
+            ["ent"] = "1"
+          }
+        },
+        {
+          id = 3,
+          name = "Collect3",
+          type = "pickup",
+          shape = "rectangle",
+          x = 56,
+          y = 28,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "1",
+            ["ent"] = "3"
+          }
+        },
+        {
+          id = 4,
+          name = "Collect4",
+          type = "pickup",
+          shape = "rectangle",
+          x = 504,
+          y = 28,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "1",
+            ["ent"] = "4"
+          }
+        },
+        {
+          id = 5,
+          name = "Collect5",
+          type = "pickup",
+          shape = "rectangle",
+          x = 504,
+          y = 504,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "1",
+            ["ent"] = "5"
+          }
+        },
+        {
+          id = 6,
+          name = "Collect6",
+          type = "pickup",
+          shape = "rectangle",
+          x = 56,
+          y = 504,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "2",
+            ["ent"] = "6"
+          }
+        },
+        {
+          id = 7,
+          name = "Collect8",
+          type = "pickup",
+          shape = "rectangle",
+          x = 252,
+          y = 28,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "2",
+            ["ent"] = "8"
+          }
+        },
+        {
+          id = 8,
+          name = "Collect2",
+          type = "pickup",
+          shape = "rectangle",
+          x = 112,
+          y = 308,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0",
+            ["ent"] = "2"
+          }
+        },
+        {
+          id = 9,
+          name = "Collect7",
+          type = "pickup",
+          shape = "rectangle",
+          x = 252,
+          y = 420,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "2",
+            ["ent"] = "7"
+          }
+        }
+      }
+    }
+  }
+}
