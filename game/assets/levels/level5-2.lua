8fa93ed264d260b122082e6a0ce3d08f77632f9f
Selcopa <selcopa@gmail.com>
2017-01-21 02:05:21 -0800

    level 5-2 WIP
    level 5-2 WIP

---
+++ game/assets/levels/level5-2.lua
@@ -0,0 +1,133 @@
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  tiledversion = "0.18.0",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 20,
+  height = 20,
+  tilewidth = 28,
+  tileheight = 28,
+  nextobjectid = 18,
+  properties = {},
+  tilesets = {},
+  layers = {
+    {
+      type = "objectgroup",
+      name = "Object Layer 1",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      draworder = "topdown",
+      properties = {},
+      objects = {
+        {
+          id = 12,
+          name = "Collect0",
+          type = "pickup",
+          shape = "rectangle",
+          x = 28,
+          y = 196,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0",
+            ["ent"] = "0"
+          }
+        },
+        {
+          id = 13,
+          name = "Collect0",
+          type = "pickup",
+          shape = "rectangle",
+          x = 252,
+          y = 0,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0",
+            ["ent"] = "0"
+          }
+        },
+        {
+          id = 14,
+          name = "Collect0",
+          type = "pickup",
+          shape = "rectangle",
+          x = 504,
+          y = 336,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0",
+            ["ent"] = "0"
+          }
+        },
+        {
+          id = 15,
+          name = "Collect0",
+          type = "pickup",
+          shape = "rectangle",
+          x = 308,
+          y = 504,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0",
+            ["ent"] = "0"
+          }
+        },
+        {
+          id = 16,
+          name = "Collect0",
+          type = "pickup",
+          shape = "rectangle",
+          x = 56,
+          y = 84,
+          width = 28,
+          height = 28,
+          rotation = 0,
+          visible = true,
+          properties = {
+            ["chunk"] = "0",
+            ["ent"] = "0"
+          }
+        },
+        {
+          id = 17,
+          name = "",
+          type = "",
+          shape = "polyline",
+          x = 84,
+          y = 224,
+          width = 0,
+          height = 0,
+          rotation = 0,
+          visible = true,
+          polyline = {
+            { x = 0, y = 0 },
+            { x = 196, y = -168 },
+            { x = 392, y = 112 },
+            { x = 224, y = 252 },
+            { x = 28, y = -112 },
+            { x = 364, y = -196 },
+            { x = 448, y = -84 },
+            { x = 56, y = 196 },
+            { x = 0, y = 84 },
+            { x = 0, y = 0 }
+          },
+          properties = {}
+        }
+      }
+    }
+  }
+}
