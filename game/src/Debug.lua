02ad2c4ef41b33a6a4b789ec56ad077d032fb1dc
Cody Malach <codymalach@gmail.com>
2017-01-21 18:40:11 -0800

    Add draw rectangle shape debug function.

--- game/src/Debug.lua
+++ game/src/Debug.lua
@@ -11,4 +11,16 @@ Debug.drawCircleShape = function(body, shape)
 	love.graphics.circle("fill", x, y, r);
 end
 
+Debug.drawRectangleShape = function(body, width, height)
+	local x, y = body:getPosition();
+	local angle = body:getAngle();
+	love.graphics.push()
+	love.graphics.translate(x, y);
+	love.graphics.rotate(angle);
+
+	love.graphics.setColor(255, 255, 0, 150);
+	love.graphics.rectangle("fill", -width / 2, -height / 2, width, height);
+	love.graphics.pop();	
+end
+
 return Debug;
